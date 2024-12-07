f2f0d4da62a1cc15f6d7df3bc691c07708e2fb29
JiHoon777 <thdwlgnsl@naver.com>
2024-12-07 21:52:44 +0900

    feat: email 일때 골뱅이 이전까지만 표시

--- src/pages/home/ui/HomeGreeting.tsx
+++ src/pages/home/ui/HomeGreeting.tsx
@@ -1,9 +1,16 @@
+'use client'
+import { useUserStore } from '@/entities/user'
+
 import { getGreetingByTime } from '../lib/getGreetingByTime'
 
 export const HomeGreeting = () => {
+  const user = useUserStore((state) => state.user)
+
   return (
     <div className={'w-full'}>
-      <h2 className="mb-3 text-3xl font-bold">유저님, 안녕하세요 👋</h2>
+      <h2 className="mb-3 text-3xl font-bold">
+        {user?.name ?? user?.email?.split('@')[0] ?? '유저'}님, 안녕하세요 👋
+      </h2>
       <p className="text-lg font-semibold text-gray-600">
         {getGreetingByTime()}
       </p>
