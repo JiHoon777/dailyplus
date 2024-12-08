0636ff828f0c25b34873d9b91a06b0922b8b6e2a
JiHoon777 <thdwlgnsl@naver.com>
2024-12-08 20:55:40 +0900

    feat: 아티클 생성 모달

--- src/pages/admin/posts/ui/createArticleWithAiModal.tsx
+++ src/pages/admin/posts/ui/createArticleWithAiModal.tsx
@@ -1,5 +1,9 @@
-import { ModalOverlay } from '@/shared/ui'
+'use client'
+import { ArticleType } from '@/shared/types'
+import { Button, Label, ModalOverlay } from '@/shared/ui'
+import { RadioGroup, RadioGroupItem } from '@/shared/ui/radio-group'
 
+// Todo: Form Type Check, like zod
 export const CreateArticleWithAiModal = ({
   isOpen,
   onClose,
@@ -7,9 +11,45 @@ export const CreateArticleWithAiModal = ({
   isOpen: boolean
   onClose: () => void
 }) => {
+  const handleCreate = () => {}
+
+  const articleTypes: ArticleType[] = [
+    'trend',
+    'it',
+    'ai',
+    'front-end',
+    'back-end',
+    'korea-news',
+    'world-news',
+  ]
+
   return (
-    <ModalOverlay isOpen={isOpen} onClose={onClose}>
-      <header>Create Article With Perplexity</header>
+    <ModalOverlay
+      isOpen={isOpen}
+      onClose={onClose}
+      className="flex flex-col gap-6 p-4"
+    >
+      <header>
+        <h3 className="text-lg font-semibold">
+          Create Article With Perplexity
+        </h3>
+      </header>
+      <div>
+        <RadioGroup defaultValue="trend" className="gap-2">
+          {articleTypes.map((articleType) => (
+            <div key={articleType} className="flex items-center space-x-2">
+              <RadioGroupItem value={articleType} id={articleType} />
+              <Label htmlFor={articleType}>{articleType}</Label>
+            </div>
+          ))}
+        </RadioGroup>
+      </div>
+      <footer className="flex w-full justify-end gap-4">
+        <Button variant="ghost" onClick={onClose}>
+          Close
+        </Button>
+        <Button onClick={handleCreate}>Submit</Button>
+      </footer>
     </ModalOverlay>
   )
 }
