ca655879120e691632c067ec9a2361b2b3b54528
JiHoon777 <thdwlgnsl@naver.com>
2024-12-14 21:28:34 +0900

    refactor: autoPlayCarousel

--- src/widgets/carousel/autoPlayCarousel.tsx
+++ src/widgets/carousel/autoPlayCarousel.tsx
@@ -3,7 +3,11 @@ import type { EmblaOptionsType } from 'embla-carousel'
 
 import AutoScroll from 'embla-carousel-auto-scroll'
 import useEmblaCarousel from 'embla-carousel-react'
-import { useRef } from 'react'
+
+const DEFAULT_OPTIONS: EmblaOptionsType = {
+  align: 'start',
+  loop: true,
+}
 
 export const AutoPlayCarousel = <T,>({
   slides,
@@ -14,41 +18,22 @@ export const AutoPlayCarousel = <T,>({
   options?: EmblaOptionsType
   children: (item: T, index: number) => React.ReactNode
 }) => {
-  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
-    AutoScroll({ playOnInit: true }),
+  const [emblaRef] = useEmblaCarousel({ ...DEFAULT_OPTIONS, ...options }, [
+    AutoScroll({
+      playOnInit: true,
+      speed: 1,
+      stopOnFocusIn: true,
+      stopOnInteraction: false,
+      stopOnMouseEnter: true,
+    }),
   ])
-  const isAutoPlayingRef = useRef(true)
-
-  const autoPlay = () => {
-    const autoScroll = emblaApi?.plugins()?.autoScroll
-    if (!autoScroll || isAutoPlayingRef.current) {
-      return
-    }
-
-    autoScroll.play()
-    isAutoPlayingRef.current = true
-  }
-  const stopAutoPlay = () => {
-    const autoScroll = emblaApi?.plugins()?.autoScroll
-    if (!autoScroll || isAutoPlayingRef.current) {
-      return
-    }
-
-    autoScroll.stop()
-    isAutoPlayingRef.current = false
-  }
 
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {slides.map((slide, index) => (
-            <div
-              className="embla__slide"
-              key={index}
-              onMouseEnter={stopAutoPlay}
-              onMouseLeave={autoPlay}
-            >
+            <div className="embla__slide" key={index}>
               {children(slide, index)}
             </div>
           ))}
