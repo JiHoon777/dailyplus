e96e6d5f5fa15b3b04286c101799f8b4b3f0e538
JiHoon777 <thdwlgnsl@naver.com>
2024-12-17 21:36:34 +0900

    feat: infiniteScrollLoader

--- src/shared/lib/loader/infiniteScrollLoader.tsx
+++ src/shared/lib/loader/infiniteScrollLoader.tsx
@@ -1,3 +1,74 @@
-export const InfiniteScrollLoader = () => {
+import type { ListableParams } from '.'
+
+import { type QueryKey, useInfiniteQuery } from '@tanstack/react-query'
+
+export const InfiniteScrollLoader = <
+  TData,
+  TParams extends Record<string, unknown>,
+>({
+  fetchData,
+  params,
+  queryKey,
+  children,
+}: {
+  fetchData: (options: ListableParams<TParams>) => Promise<{
+    list: TData[]
+    totalCount: number
+    error: Error | null
+  }>
+  params: Omit<ListableParams<TParams>, 'page'>
+  queryKey: (options: Omit<ListableParams<TParams>, 'page'>) => QueryKey
+  children: (list: TData[]) => React.ReactNode
+}) => {
+  const {
+    data,
+    isLoading,
+    isFetchingNextPage,
+    hasNextPage,
+    fetchNextPage,
+    error,
+  } = useInfiniteQuery<
+    {
+      list: TData[]
+      totalCount: number
+      error: Error | null
+    },
+    Error,
+    {
+      list: TData[]
+      totalCount: number
+      error: Error | null
+    },
+    QueryKey,
+    number
+  >({
+    getNextPageParam: (lastPage, allPages) => {
+      const totalPages = Math.ceil(lastPage.totalCount / params.limit)
+      const nextPage = allPages.length + 1
+
+      return nextPage <= totalPages ? nextPage : undefined
+    },
+    initialPageParam: 1,
+    queryFn: async ({ pageParam = 1 }) => {
+      const result = await fetchData({
+        ...params,
+        page: pageParam,
+      })
+
+      if (result.error) {
+        throw result.error
+      }
+
+      return result
+    },
+    queryKey: queryKey(params),
+  })
+
+  if (error) {
+    return <div>Error: {error.toString()}</div>
+  }
+
+  const flattenedList = data?.list.flatMap((page) => page) ?? []
+
   return <div>loading...</div>
 }
